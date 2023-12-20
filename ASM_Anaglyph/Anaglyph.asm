e6bb63dceee05d39b9c86306d5b36b48521a294a
xTaromarux <slawekput4@gmail.com>
2023-12-20 03:11:04 +0100

    Improvement with matrix multiplication in ASM

--- ASM_Anaglyph/Anaglyph.asm
+++ ASM_Anaglyph/Anaglyph.asm
@@ -1,41 +1,42 @@
 .code
 
-matrix_multiply proc
-    ; Parameters:
-    ; [rdi] - matrix1
-    ; [rsi] - matrix2
-    ; [rdx] - result
+    matrix_multiply proc
+        ; Parameters:
+        ; [rcx] - matrix1 3x3
+        ; [rdx] - matrix2 1x3
+        ; [r8] - result 1x3
 
-    ; Initialize variables
-    xorpd xmm4, xmm4       ; Clear xmm4 to store the result for the current row
-    xor rax, rax           ; rax will be used for outer loop counters
+        ; Initialize variables
+        xorpd xmm4, xmm4       ; Clear xmm4 to store the result for the current row
+        xor rax, rax             ; rax will be used for outer loop counters
+        outer_loop:               ; Outer loop (rows of matrix1)
+            xor rdi, rdi             ; rax will be used for inner loop counters
+            
+            cmp rax, 3            ; Check if outer loop is done (assuming matrix1 is 3x3)
+            je Done               ; End of outer loop
 
-    ; Outer loop (rows of matrix1)
-    outer_loop:
-        mov rcx, 0          ; rcx will be used for inner loop counters
+        inner_loop:                  ; Inner loop (columns of matrix2)
+            movdqu xmm0, [rcx]       ; Load matrix1 element to xmm0
+            movdqu xmm1, [rdx]       ; Load matrix2 element to xmm1
+            pmullw xmm1, xmm0        ; Multiply matrix1 and matrix2 elements
+            paddusw xmm4, xmm1       ; Add the result to xmm4
 
-        ; Inner loop (columns of matrix2)
-        inner_loop:
-            movsd xmm0, qword ptr [rdi + rax * 8]       ; Load matrix1 element to xmm0
-            movsd xmm1, qword ptr [rsi + rcx * 8]       ; Load matrix2 element to xmm1
-            mulsd xmm1, xmm0                            ; Multiply matrix1 and matrix2 elements
-            addsd xmm4, xmm1                            ; Add the result to xmm4
+   
+            inc rdi                  ; Move to the next column of matrix2
+            cmp rdi, 2               ; Check if inner loop is done (assuming matrix2 is 3x3)
+            je innerLoopDone         ; End of inner loop
+            jmp inner_loop
 
-            inc rcx                                     ; Move to the next column of matrix2
-            cmp rcx, 1                                  ; Check if inner loop is done (assuming matrix2 is 3x3)
-            jl inner_loop
-        ; End of inner loop
+        innerLoopDone:
+           inc rax               ; Move to the next row of matrix1
+           movdqu [r8], xmm4     ; Store the result in the result matrix
+           xorpd xmm4, xmm4      ; Clear xmm4 for the next iteration
+           jmp outer_loop
 
-        ; Store the result in the result matrix
-        movsd qword ptr [rdx + rax * 8], xmm4
-        xorpd xmm4, xmm4      ; Clear xmm4 for the next iteration
-        inc rax               ; Move to the next row of matrix1
-        cmp rax, 3            ; Check if outer loop is done (assuming matrix1 is 3x3)
-        jl outer_loop
-    ; End of outer loop
+        Done: 
+            ret 
 
-    ret
-matrix_multiply endp
+    matrix_multiply endp
 
 end
     
--- Anaglyph/Program.cs
+++ Anaglyph/Program.cs
@@ -4,22 +4,21 @@ using System.Runtime.InteropServices;
 class Program
 {
     [DllImport("C:\\Users\\slawek\\source\\repos\\Anaglyph\\x64\\Debug\\ASM_Anaglyph.dll")]
-    public static extern void matrix_multiply(float[] matrix1, float[] matrix2, float[] result);
+    public static extern void matrix_multiply(Int16[] matrix1, Int16[] matrix2, Int16[] result);
 
     static void Main()
     {
-        float[] matrix1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        float[] matrix2 = { 1, 2, 3 };
-        float[] result = new float[3];
+        Int16[] matrix1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Int16[] matrix2 = { 1, 2, 3 } ;
+        Int16[] result = { 0, 0, 0 };
 
         matrix_multiply(matrix1, matrix2, result);
 
         Console.WriteLine("Wynik mnożenia macierzy:");
-        for (int i = 0; i < result.Length; i++)
+        foreach (float b in result)
         {
-            Console.WriteLine(result[i]);
+            Console.Write(b + "\n");
         }
-
-        Console.ReadLine();
+        Console.WriteLine();
     }
 }
