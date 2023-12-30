54e333e3a0fc0bd2d4a459fa5576892eb786e39b
Gosiatomilo <gosiatomilo@gmail.com>
2023-12-31 00:02:29 +0100

    Dodanie przycisku "Generuj" oraz kodu wyświetlającego w textBoxie losową liczbe w czasie wykonywania programu

--- Anaglyph/Form1.Designer.cs
+++ Anaglyph/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -74,11 +76,32 @@
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(598, 451);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Generuj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 462);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Czas wykonywania to:";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(860, 475);
+            this.ClientSize = new System.Drawing.Size(860, 523);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox2);
@@ -98,6 +121,8 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 // aaa
--- Anaglyph/Form1.cs
+++ Anaglyph/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -26,5 +27,23 @@ namespace Anaglyph
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Tworzymy obiekt do generowania liczb losowych
+            Random random = new Random();
+
+            // Generujemy losową liczbę
+            int wygenerowanaLiczba = random.Next();
+
+            // Możemy także wyświetlić wygenerowaną liczbę
+            textBox1.Text = "Czas wykonania to: " + wygenerowanaLiczba + " ms";
+        }
+
     }
 }
+
