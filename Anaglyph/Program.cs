1393bb86bbc9c59b4d95621ecf5f2146dace3ce0
Gosiatomilo <gosiatomilo@gmail.com>
2024-01-07 00:12:17 +0100

    Angorytm tworzący zdjęcie wynikowe w języku wysokiego poziomu (c#)

--- Anaglyph/Program.cs
+++ Anaglyph/Program.cs
@@ -84,5 +84,69 @@ class Program
             Console.Write(res + "\n");
         }
         Console.WriteLine();
+
+        Bitmap bitmapOfFirstImage = new Bitmap("C:\\Users\\slawek\\source\\repos\\Anaglyph\\Anaglyph\\Resources\\FirstImage.jpg");
+        Bitmap bitmapOfSecondImage = new Bitmap("C:\\Users\\slawek\\source\\repos\\Anaglyph\\Anaglyph\\Resources\\SecondImage.jpg");
+        Bitmap resultBitmap = new Bitmap(bitmapOfFirstImage.Width, bitmapOfFirstImage.Height);
+        // Sprawdź, czy obie bitmapy mają takie same rozmiary
+        if (bitmapOfFirstImage.Width != bitmapOfSecondImage.Width || bitmapOfFirstImage.Height != bitmapOfSecondImage.Height)
+        {
+            Console.WriteLine("Obie bitmapy muszą mieć takie same rozmiary.");
+            return;
+        }
+
+        // Przygotuj macierze do mnożenia
+        double[,] matrix1 = { { 1, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+        double[,] matrix2 = { { 0, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+
+        // Przetwórz każdy piksel
+        for (int x = 0; x < bitmapOfFirstImage.Width; x++)
+        {
+            for (int y = 0; y < bitmapOfFirstImage.Height; y++)
+            {
+                Color pixel1 = bitmapOfFirstImage.GetPixel(x, y);
+                Color pixel2 = bitmapOfSecondImage.GetPixel(x, y);
+
+                // Wykonaj mnożenie macierzowe i dodawanie
+                double[] resultArray = MatrixMultiply(matrix1, new double[] { pixel1.R, pixel1.G, pixel1.B });
+                resultArray = VectorAdd(resultArray, MatrixMultiply(matrix2, new double[] { pixel2.R, pixel2.G, pixel2.B }));
+
+                // Ustaw wynikowy piksel w wynikowej bitmapie
+                resultBitmap.SetPixel(x, y, Color.FromArgb((int)resultArray[0], (int)resultArray[1], (int)resultArray[2]));
+            }
+        }
+
+        // Zapisz wynikową bitmapę
+        resultBitmap.Save("C:\\Users\\slawek\\source\\repos\\Anaglyph\\Anaglyph\\Resources\\ResultImage.jpg");
+    }
+
+    // Funkcja do mnożenia macierzy 3x3 przez wektor 3x1
+    static double[] MatrixMultiply(double[,] matrix, double[] vector)
+    {
+        double[] result = new double[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            result[i] = 0;
+            for (int j = 0; j < 3; j++)
+            {
+                result[i] += matrix[i, j] * vector[j];
+            }
+        }
+
+        return result;
+    }
+
+    // Funkcja do dodawania dwóch wektorów
+    static double[] VectorAdd(double[] vector1, double[] vector2)
+    {
+        double[] result = new double[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            result[i] = vector1[i] + vector2[i];
+        }
+
+        return result;
     }
 }
