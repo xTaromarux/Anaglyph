52e4f83f2c34b0016e9c8d5df26d0befffdc06ad
xTaromarux <slawekput4@gmail.com>
2024-01-08 21:08:44 +0100

    register changes

--- ASM_Anaglyph/Anaglyph.asm
+++ ASM_Anaglyph/Anaglyph.asm
@@ -6,20 +6,20 @@
         ; rcx - result bmp
         ; rdx - original bmp no.1
         ; r8  - original bmp no.2
-        ; r9 - image size in bites
+        ; r9d - image size in bites
 
         ; Initialize variables
-        mov rsi, 0                                  ; rsi will be used for pixel counter (BGR)
+        mov rax, 0                                  ; rax will be used for pixel counter (BGR)
         mov r10, 0                                  ; r10 will be used for index counter
 
         bmp_loop:                                   ; Main loop 
-            cmp rsi, 2                              ; Check whether to change the bmp 
+            cmp rax, 2                              ; Check whether to change the bmp 
             je change_bmp                           ; Jump if true
 
             mov al, byte ptr [r8 + r10]             ; Store the byte from bmp no.2 in al
             mov byte ptr [rcx + r10], al            ; Move al to bmp result
             inc r10                                 ; Move to the next byte in the array
-            inc rsi                                 ; Move to the next byte of pixel (BGR) in the array
+            inc rax                                 ; Move to the next byte of pixel (BGR) in the array
             dec r9d                                 ; Decrement image size = loop counter  
             jnz bmp_loop                            ; Continue loop if r9 is not 0
             ret                                     ; Stop procedure
@@ -28,7 +28,7 @@
         change_bmp:                                 ; Change to bmp no.1
             mov al, byte ptr [rdx + r10]            ; Store the byte from bmp no.1 in al
             mov byte ptr [rcx + r10], al            ; Move al to bmp result
-            mov rsi, 0                              ; Reset pixel counter 
+            mov rax, 0                              ; Reset pixel counter 
             inc r10                                 ; Move to the next byte in the array
             dec r9d                                 ; Decrement image size = loop counter
             jnz bmp_loop                            ; Continue loop if r9 is not 0
